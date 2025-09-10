f02334b530d6f652bd0feeea524ab4d2e87a6137
Dudko Mikhail <dudkomikhail01@gmail.com>
2025-09-10 11:47:42 +0300

    finish SameTree and InvertBinaryTree tasks

---
+++ src/main/java/by/dudko/education/algorithm/leetcode150/binarytree/InvertBinaryTree.java
@@ -0,0 +1,43 @@
+package by.dudko.education.algorithm.leetcode150.binarytree;
+
+/**
+ * https://leetcode.com/problems/invert-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
+ * <p>
+ * 226. Invert Binary Tree
+ * <p>
+ * Given the root of a binary tree, invert the tree, and return its root.
+ * <p>
+ * Example 1:
+ * Input: root = [4,2,7,1,3,6,9]
+ * Output: [4,7,2,9,6,3,1]
+ * <p>
+ * Example 2:
+ * Input: root = [2,1,3]
+ * Output: [2,3,1]
+ * <p>
+ * Example 3:
+ * Input: root = []
+ * Output: []
+ * <p>
+ * Constraints:
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        invertTreeHelper(root);
+        return root;
+    }
+
+    private void invertTreeHelper(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTreeHelper(root.left);
+        invertTreeHelper(root.right);
+    }
+}
---
+++ src/main/java/by/dudko/education/algorithm/leetcode150/binarytree/SameTree.java
@@ -0,0 +1,38 @@
+package by.dudko.education.algorithm.leetcode150.binarytree;
+
+/**
+ * https://leetcode.com/problems/same-tree/description/?envType=study-plan-v2&envId=top-interview-150
+ * <p>
+ * 100. Same Tree
+ * <p>
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+ * <p>
+ * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+ * <p>
+ * Example 1:
+ * Input: p = [1,2,3], q = [1,2,3]
+ * Output: true
+ * <p>
+ * Example 2:
+ * Input: p = [1,2], q = [1,null,2]
+ * Output: false
+ * <p>
+ * Example 3:
+ * Input: p = [1,2,1], q = [1,1,2]
+ * Output: false
+ * <p>
+ * Constraints:
+ * The number of nodes in both trees is in the range [0, 100].
+ * -10^4 <= Node.val <= 10^4
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode first, TreeNode second) {
+        if (first == null || second == null) {
+            return first == second;
+        }
+
+        return first.val == second.val
+                && isSameTree(first.left, second.left)
+                && isSameTree(first.right, second.right);
+    }
+}
