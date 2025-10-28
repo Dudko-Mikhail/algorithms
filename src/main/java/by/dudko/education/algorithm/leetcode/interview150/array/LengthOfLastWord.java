47bc03ccad92754a01a8985678faa9ab0282338d
Dudko Mikhail <dudkomikhail01@gmail.com>
2025-10-28 16:38:15 +0300

    Solve LengthOfLastWord problem

---
+++ src/main/java/by/dudko/education/algorithm/leetcode/interview150/array/LengthOfLastWord.java
@@ -0,0 +1,40 @@
+package by.dudko.education.algorithm.leetcode.interview150.array;
+
+/**
+ * Given a string s consisting of words and spaces, return the length of the last word in the string.
+ * A word is a maximal substring consisting of non-space characters only.
+ * <p>
+ * Example 1:
+ * Input: s = "Hello World"
+ * Output: 5
+ * Explanation: The last word is "World" with length 5.
+ * <p>
+ * Example 2:
+ * Input: s = "   fly me   to   the moon  "
+ * Output: 4
+ * Explanation: The last word is "moon" with length 4.
+ * <p>
+ * Example 3:
+ * Input: s = "luffy is still joyboy"
+ * Output: 6
+ * Explanation: The last word is "joyboy" with length 6.
+ * <p>
+ * Constraints:
+ * 1 <= s.length <= 10^4
+ * s consists of only English letters and spaces ' '.
+ * There will be at least one word in s.
+ */
+public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        int end = i;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            i--;
+        }
+        return end - i;
+    }
+}
